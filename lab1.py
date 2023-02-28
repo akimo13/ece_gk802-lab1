ec09d1e69f178cfaee47f430638898ac591c06eb
Dimitris Karatsoulas <d.karatsoulas@gmail.com>
2023-02-28 11:22:55 +0200

    1st exercise solution, Draft, added extra comments

--- lab1.py
+++ lab1.py
@@ -15,19 +15,19 @@ response = requests.request('GET', url)
 
 # Printing HTTP response headers
 print("\nHere are the HTTP response headers:\n")
-for header in response.headers:                                         # Getting HTTP headers from response object
-    if header.lower() != "set-cookie" and header.lower() != "server":   # excluding Cookies and Server, using lower() because HTTP headers are case-insensitive
+for header in response.headers:                                                 # Getting HTTP headers from response object
+    if header.lower() != "set-cookie" and header.lower() != "server":           # excluding Cookies and Server, using lower() because HTTP headers are case-insensitive
         value = response.headers.get(header)
         print(f"{header}: {value}")
 
 # Printing information for software server
-software = response.headers.get('Server')
+software = response.headers.get('Server')                                       # Getting Server information from response object
 if software:
     print(f"\nThe software used by the server is: {software}")
 
 # Printing information for cookies
 print("\nHere are the cookies used by the webpage:\n")
-for cookie in response.cookies:
+for cookie in response.cookies:                                                 # Getting Cookies information from response object
         expiration_time = time.localtime(cookie.expires)                        # Converting seconds timestamp to a Python time tuple
         expiration_date = time.strftime("%Y-%m-%d %H:%M:%S", expiration_time)   # Converting Python time tuple to a readable string
         print(f"{cookie.name}, expiring at {expiration_date}")
