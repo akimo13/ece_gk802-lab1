76702c7e7be827ed42e3ac910fa946c6e4d22383
Dimitris Karatsoulas <d.karatsoulas@gmail.com>
2023-03-02 19:27:38 +0200

    Final commit, added reg ex, extra features.

--- lab1-1.py
+++ lab1-1.py
@@ -1,33 +1,50 @@
-# importing requests library
+# HCI202, Design of Web Applications, 1st Lab Exercise
+# 1103932, Karatsoulas Dimitrios
+
+# importing libraries
 import requests
-#importing time Python module to convert seconds to a human readable date
-import time
+import re
+import time                                                                        # Importing time Python module to convert seconds to a human readable date
 
-# Asking URL from user
-url = input("Please enter a valid URL: ")
+ # While loop enables program to keep asking for a valid URL until it gets one
+while True:
+    # Prompting for a valid URL
+    url = input("Please enter a valid URL: ")
+    if not url.startswith("http"):                                                  # Adding "https://" if it's not in front of the url
+        url = "https://" + url
 
-# Checking if URL starts with https or http, if not it adds https:// 
-if not url.startswith("https://") or url.startswith("http://"):
-    url = "https://" + url
+    valid_url_pattern = re.compile(
+        r'^(?:http|ftp)s?://'                                                       # Checking for http:// or https://
+        r'(?:www\.)?'                                                               # Checking for "www."
+        r'\w+\.[a-zA-Z]{2,}(?:\.\w+)*'                                              # Checking for domain names and top-level domain
+    )
+    
+    # Validation for URL, if not asking for a valid one 
+    if not re.match(valid_url_pattern, url):
+        print("Please try to input a valid URL.")
+        continue
 
-# Making a URL request and getting a response object
-response = requests.request('GET', url)
+    # Making a URL request and getting a response object   
+    response = requests.get(url)
 
-# Printing HTTP response headers
-print("\nHere are the HTTP response headers:\n")
-for header in response.headers:                                                 # Getting HTTP headers from response object
-    if header.lower() != "set-cookie" and header.lower() != "server":           # excluding Cookies and Server, using lower() because HTTP headers are case-insensitive
-        value = response.headers.get(header)
-        print(f"{header}: {value}")
+    # Printing HTTP response headers
+    print("\nHere are the HTTP response headers:\n")
+    for header in response.headers:                                                 # Getting HTTP headers from response object
+        if header.lower() != "set-cookie" and header.lower() != "server":           # excluding Cookies and Server, using lower() because HTTP headers are case-insensitive
+            value = response.headers.get(header)
+            print(f"{header}: {value}")
 
-# Printing information for software server
-software = response.headers.get('Server')                                       # Getting Server information from response object
-if software:
-    print(f"\nThe software used by the server is: {software}")
+    # Printing information for software server
+    software = response.headers.get('Server')                                       # Getting Server information from response object
+    if software:
+        print(f"\nThe server software used by {url} is: {software}")
 
-# Printing information for cookies
-print("\nHere are the cookies used by the webpage:\n")
-for cookie in response.cookies:                                                 # Getting Cookies information from response object
-        expiration_time = time.localtime(cookie.expires)                        # Converting seconds timestamp to a Python time tuple
-        expiration_date = time.strftime("%Y-%m-%d %H:%M:%S", expiration_time)   # Converting Python time tuple to a readable string
-        print(f"{cookie.name}, expiring at {expiration_date}")
+    # Printing information for cookies
+    print("\nHere are the cookies used by the webpage:\n")
+    if response.cookies:                                                            # Checking if there are cookies   
+        for cookie in response.cookies:                                             # Getting Cookies information from response object
+            expiration_time = time.localtime(cookie.expires)                        # Converting seconds timestamp to a Python time tuple
+            expiration_date = time.strftime("%Y-%m-%d %H:%M:%S", expiration_time)   # Converting Python time tuple to a readable string
+            print(f"{cookie.name}, expiring at {expiration_date}")
+    else:
+        print(f"{url} has no cookies\n")        
